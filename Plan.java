8d17b4eae0529d5f5b952032795a97de55e4bc42
derekbar90 <derekbarrera@gmail.com>
2014-09-10 18:09:57 -0400

    JavaDoc and start of a catch
    Computer crashed mid method

--- Plan.java
+++ Plan.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class Plan{
 
+	/**
+	 * Reads a file location in the @param inputLocation and 
+	 * creates a File object which is then avalible to be passed
+	 * around.
+	 * 
+	 * @param  inputLocation String which contains a file location
+	 * @return               File object of the given file location
+	 */
+	
 	public File readFile(String inputLocation){
 
 		File inputFile = new File(inputLocation);
@@ -11,5 +20,19 @@ public class Plan{
 		return inputFile;
 	
 	}
+
+	public createPlan(File planFile){
+
+		try {
+
+
+		}catch(FileNotFoundException e){
+
+			e.
+		}
+
+	}
+
+
 	
 }
