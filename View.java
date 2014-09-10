46487c20c34359b51966f5ffc15a25fe409ff4ac
derekbar90 <derekbarrera@gmail.com>
2014-09-10 13:44:02 -0400

    Add Method for Plan Of Study
    exsistingPlanOfStudy() to determine if there is existing plan of study
    file

--- View.java
+++ View.java
@@ -1,5 +1,35 @@
+import java.util.*;
+
 public class View{
 
+	public boolean exsistingPlanOfStudy(){
 
+		boolean exsistingPlanOfStudyAnswer = false;
+
+		System.out.print("Is there an existing Program of Study (Y or N)? ");
+
+		Scanner scan = new Scanner(System.in);
+
+		char decision = scan.next().charAt(0);
+
+		if (decision == 'Y' || decision == 'y') {
+
+			exsistingPlanOfStudyAnswer = true;
+			scan.close();
+			
+		}else if(decision == 'N' || decision == 'n'){
+
+			exsistingPlanOfStudyAnswer = false;
+			scan.close();
+
+		}else{
+
+			System.out.println("Please enter Y or N");
+			exsistingPlanOfStudy();
+		}
+
+		return exsistingPlanOfStudyAnswer;
+
+	}	
 
 }
